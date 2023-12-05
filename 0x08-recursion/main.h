d0970992f1ea33f92ab50f08a57e118a7eadb624
tarikaziz254 <tarikaziz2242@gmai.com>
2023-12-05 04:28:23 -0800

    reverse the number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #ifdefine MAIN_H
 
-void puts_recursion_(char*s);
-int_purchar(char c);
-void_print_rev_recursion(char*s);
-int_strlen_recursion(char*s);
+void puts_recursion_(char *s);
+int_putchar(char c);
+void_print_rev_recursion(char *s);
+int_strlen_recursion(char *s);
 int factorial(int n);
 int_pow_recursion(int x,int y);
 int_sqrt_recursion(int n);
