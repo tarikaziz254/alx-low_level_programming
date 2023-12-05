5a09710ff6b1004e38c47e93ca164a86466c2ba7
tarikaziz254 <tarikaziz2242@gmai.com>
2023-12-05 04:04:59 -0800

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,12 +2,12 @@
 #ifdefine MAIN_H
 
 void puts_recursion_(char*s);
-//int_purchar(char c);
-//void_print_rev_recursion(char*s);
-//int_strlen_recursion(char*s);
-//int factorial(int n);
-//int_pow_recursion(int x,int y);
-//int_sqrt_recursion(int n);
-//int is_prime_number(int n);
-//int is_palindrome(char*s);
+int_purchar(char c);
+void_print_rev_recursion(char*s);
+int_strlen_recursion(char*s);
+int factorial(int n);
+int_pow_recursion(int x,int y);
+int_sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char*s);
 #endif
